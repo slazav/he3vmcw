e35d640bc583deda1ef32e53f8d2c84f8ce64c2c
Vladislav Zavjalov <slazav@altlinux.org>
2011-02-07 17:36:31 +0300

    prog: add vft.f prog

---
+++ vft.f
@@ -0,0 +1,70 @@
+C--------------- FFT ---------------------------------------
+      PARAMETER (NNX=120000,ND=15000)
+      IMPLICIT REAL*8(A-H,O-Z)
+      REAL FF(NNX),F1(ND),F2(ND),F3(ND),TT(NNX)
+      CHARACTER*100 STR2
+      PI2=8.0*DATAN(1.0D0)             ! 2*PI
+
+      OPEN(89,FILE='FFT.PAR')
+      READ(89,*)FC
+      READ(89,*)DF
+      READ(89,*)NF
+      READ(89,*)T0
+      CLOSE(89)
+
+      II=0
+      open(88,FILE='A')
+      read(88,*, END=11) STR2
+      do NP=0,NNX
+        if (NP.GE.NNX) then
+          write (*,*)'Too many data points..'
+          close(88)
+          stop
+        endif
+        read(88,*, END=11) TTA,FFA
+        if (TTA.GT.T0) then
+          TT(II)=TTA-T0
+          FF(II)=FFA
+          II=II+1
+        endif
+      enddo
+   11 close(88)
+      WRITE(*,*)'Number of points: ', NP+1, ' used: ', II+1
+
+      DT=TT(2)-TT(1)
+      if (2*NF+1.GT.ND) then
+        write (*,*)'Too many points to calculate..'
+        stop
+      endif
+
+      AA=0.0
+      do I=0,II-1
+        AA=AA+FF(I)
+      enddo
+      AA=AA/II
+      do I=0,II-1
+        FF(I)=FF(I)-AA
+      enddo
+
+      FI=FC-DF*NF
+      do K=1,2*NF+1
+        F1(K)=0.0
+        F2(K)=0.0
+      enddo
+      open(87,FILE='FFT.RES')
+      do K=1,2*NF+1
+        FCUR=FI+DF*(K-1)
+        do I=0,II
+          TI=DT*(I-1)
+          F1(K)=F1(K)+FF(I)*DSIN(PI2*FCUR*TI)
+          F2(K)=F2(K)+FF(I)*DCOS(PI2*FCUR*TI)
+        enddo
+        F11=F1(K)**2
+        F22=F2(K)**2
+        F3(K)=DSQRT(F11+F22)
+        write(87,101)FCUR,F3(K)
+C        WRITE(87,101)FCUR,F1(K),F2(K),F3(K)
+      enddo
+  101 format(4(1PE25.16))
+      end
+
