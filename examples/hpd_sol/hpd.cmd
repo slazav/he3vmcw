1024b9f04d224385ed667d757be48d48248d476f
Vladislav Zavjalov <slazav@altlinux.org>
2018-09-25 20:10:07 +0300

    add hpd_sol/hpd.cmd - create a soliton in HPD

---
+++ examples/hpd_sol/hpd.cmd
@@ -0,0 +1,45 @@
+# Create HPD by sweeping magnetic field with a gradient,
+# then switch off the excitation and sobserve decay of HPD.
+
+set_rf_field   1.4e-3    # RF-field
+set_field_hz   100    # larmor position
+set_field_grad  0.0    # field gradient, G/cm
+set_field_quad -0.1    # field gradient, G/cm
+set_tf 1e-7
+cell_len 0.9
+npts 512
+
+set_rf_prof 0 2
+
+acc2 18
+npts 256
+mindt 1e-20
+
+start
+pnm_start
+
+tstep 1e-3
+sweep_field_hz 0 250
+
+set_rf_field  10e-3
+reset_time
+#pnm_hline
+tstep 1e-4
+sweep_field_hz -2 250
+
+set_rf_field  8e-3
+reset_time
+#pnm_hline
+write_profile
+wait 0.004
+
+reset_time
+write_profile
+wait 0.03
+
+write_profile
+save_state sol.state
+
+
+
+
