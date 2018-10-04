a9e0caf9d791a6d713002ea1d04736bf4e22acf8
Vladislav Zavjalov <slazav@altlinux.org>
2018-10-04 17:23:00 +0300

    vmcw_func.f: use named variables instead of U(i), UX(i), UXX(i)

--- vmcw_func.f
+++ vmcw_func.f
@@ -23,8 +23,10 @@ C-- F ---------- EVALUATION OF F ------------------------------------
 !       D (cm^2/s), Tf (s), T1 (s)
         real*8 WB, Cpar, dCpar, Diff, Tf, T1
 
-        real*8 UN, UNx,UNy,UNz
+        real*8 UMx,UMy,UMz, UNx,UNy,UNz, Uth, UN
         real*8 UMxm,UMym,UMzm
+        real*8 GNx,GNy,GNz, GMx,GMy,GMz, Gth
+        real*8 GGNx,GGNy,GGNz, GGMx,GGMy,GGMz, GGth
         real*8 DD45,ST,CT,CTM,CT1,CTG,UT,AUT,AF,DAF,FTN,DFTN,B
         real*8 UJX,UJY,UJZ,DJX,DJY,DJZ ! spin current J and dJ/dz
 
@@ -38,12 +40,34 @@ C       Normilize n vector length
         UNy = U(5)/UN
         UNz = U(6)/UN
 
-        UMxm = U(1)-Wr/Wz
-        UMym = U(2)
-        UMzm = U(3)-1.0D0
+        UMx = U(1)
+        UMy = U(2)
+        UMz = U(3)
+        Uth = U(7)
 
-        ST=dsin(U(7))
-        CT=dcos(U(7))
+        UMxm = UMx-Wr/Wz
+        UMym = UMy
+        UMzm = UMz-1.0D0
+
+!        GMx = UX(1)
+!        GMy = UX(2)
+!        GMz = UX(3)
+        GNx = UX(4)
+        GNy = UX(5)
+        GNz = UX(6)
+        Gth = UX(7)
+
+        GGMx = UXX(1)
+        GGMy = UXX(2)
+        GGMz = UXX(3)
+        GGNx = UXX(4)
+        GGNy = UXX(5)
+        GGNz = UXX(6)
+        GGth = UXX(7)
+
+        ST=dsin(Uth)
+        CT=dcos(Uth)
+
         CTM=1.0D0-CT
         CT1=1.0D0+CT
         CTG=ST/CTM      ! ctg(T/2) = sin(T)/(1-cos(T))
@@ -54,46 +78,46 @@ C       Normilize n vector length
         DAF = -2D0*Cpar*dCpar/Wz
 
 C       nx ny' - ny nx'
-        DD45=UNx*UX(5)-UX(4)*UNy
+        DD45=UNx*GNy-GNx*UNy
 C       (1-ct)*(nx ny' - ny nx') - st*nz' - nz t'
-        FTN=CTM*DD45-ST*UX(6)-UX(7)*UNz
+        FTN=CTM*DD45-ST*GNz-Gth*UNz
 C       FTN'
-        DFTN=CTM*(UNx*UXX(5)-UXX(4)*UNy)-ST*UXX(6)-UXX(7)*UNz-
-     *   CT1*UX(7)*UX(6)+ST*UX(7)*DD45
+        DFTN=CTM*(UNx*GGNy-GGNx*UNy)-ST*GGNz-GGth*UNz-
+     *   CT1*Gth*GNz+ST*Gth*DD45
 
 C       components of spin current, Jiz (without AF factor!)
-        UJX=2.0D0*(UX(7)*UNx+ST*UX(4)+CTM*(UNy*UX(6)-UX(5)*UNz))
+        UJX=2.0D0*(Gth*UNx+ST*GNx+CTM*(UNy*GNz-GNy*UNz))
      *     + (CTM*UNx*UNz+UNy*ST)*FTN
-        UJY=2.0D0*(UX(7)*UNy+ST*UX(5)-CTM*(UNx*UX(6)-UX(4)*UNz))
+        UJY=2.0D0*(Gth*UNy+ST*GNy-CTM*(UNx*GNz-GNx*UNz))
      *     + (CTM*UNy*UNz-UNx*ST)*FTN
-        UJZ=2.0D0*(UX(7)*UNz+ST*UX(6)+CTM*(UNx*UX(5)-UX(4)*UNy))
+        UJZ=2.0D0*(Gth*UNz+ST*GNz+CTM*(UNx*GNy-GNx*UNy))
      *     + (CTM*UNz**2+CT)*FTN
 
 C       derivative of the spin flow: d/dz(Jiz - Diff*Mi')
 C       Note: here non-uniform spin-wave velocity (DAF!=0) can be used,
 C             but spin diffusion should be uniform
-        DJX = AF*(2.0D0*(UXX(7)*UNx+CT1*UX(7)*UX(4)+ST*UXX(4)+
-     *   ST*UX(7)*(UNy*UX(6)-UX(5)*UNz)+CTM*(UNy*UXX(6)-UXX(5)*UNz))+
-     *   (CTM*UNx*UNz+UNy*ST)*DFTN+(ST*UX(7)*UNx*UNz+
-     *   CTM*(UX(4)*UNz+UNx*UX(6))+UX(5)*ST+UNy*CT*UX(7))*FTN)
-     *   + DAF*UJX - DIFF*UXX(1)
-        DJY=AF*(2.0D0*(UXX(7)*UNy+CT1*UX(7)*UX(5)+ST*UXX(5)-
-     *   ST*UX(7)*(UNx*UX(6)-UX(4)*UNz)-CTM*(UNx*UXX(6)-UXX(4)*UNz))+
-     *   (CTM*UNy*UNz-UNx*ST)*DFTN+(ST*UX(7)*UNy*UNz+
-     *   CTM*(UX(5)*UNz+UNy*UX(6))-UX(4)*ST-UNx*CT*UX(7))*FTN)
-     *   + DAF*UJY - DIFF*UXX(2)
-        DJZ=AF*(2.0D0*(UXX(7)*UNz+CT1*UX(7)*UX(6)+ST*UXX(6)+
-     *   ST*UX(7)*DD45+CTM*(UNx*UXX(5)-UXX(4)*UNy))+
-     *   (CTM*UNz**2+CT)*DFTN+(ST*UX(7)*UNz**2+
-     *   CTM*2.0D0*UNz*UX(6)-ST*UX(7))*FTN)
-     *   + DAF*UJZ - DIFF*UXX(3)
+        DJX = AF*(2.0D0*(GGth*UNx+CT1*Gth*GNx+ST*GGNx+
+     *   ST*Gth*(UNy*GNz-GNy*UNz)+CTM*(UNy*GGNz-GGNy*UNz))+
+     *   (CTM*UNx*UNz+UNy*ST)*DFTN+(ST*Gth*UNx*UNz+
+     *   CTM*(GNx*UNz+UNx*GNz)+GNy*ST+UNy*CT*Gth)*FTN)
+     *   + DAF*UJX - DIFF*GGMx
+        DJY=AF*(2.0D0*(GGth*UNy+CT1*Gth*GNy+ST*GGNy-
+     *   ST*Gth*(UNx*GNz-GNx*UNz)-CTM*(UNx*GGNz-GGNx*UNz))+
+     *   (CTM*UNy*UNz-UNx*ST)*DFTN+(ST*Gth*UNy*UNz+
+     *   CTM*(GNy*UNz+UNy*GNz)-GNx*ST-UNx*CT*Gth)*FTN)
+     *   + DAF*UJY - DIFF*GGMy
+        DJZ=AF*(2.0D0*(GGth*UNz+CT1*Gth*GNz+ST*GGNz+
+     *   ST*Gth*DD45+CTM*(UNx*GGNy-GGNx*UNy))+
+     *   (CTM*UNz**2+CT)*DFTN+(ST*Gth*UNz**2+
+     *   CTM*2.0D0*UNz*GNz-ST*Gth)*FTN)
+     *   + DAF*UJZ - DIFF*GGMz
 
         B = UNx*UMxm + UMym*UNy + UMzm*UNz
 
 C       Leggett equations
-        FV(1)=   (Wz-W0)*U(2)           + AUT*UNx - DJX
-        FV(2)= - (Wz-W0)*U(1) + Wr*U(3) + AUT*UNy - DJY
-        FV(3)=                - Wr*U(2) + AUT*UNz - DJZ - UMzm/T1
+        FV(1)=   (Wz-W0)*UMy          + AUT*UNx - DJX
+        FV(2)= - (Wz-W0)*UMx + Wr*UMz + AUT*UNy - DJY
+        FV(3)=               - Wr*UMy + AUT*UNz - DJZ - UMzm/T1
         FV(4)=-W0*UNy-0.5D0*Wz*(UMzm*UNy-UMym*UNz+CTG*(B*UNx-UMxm))
         FV(5)= W0*UNx-0.5D0*Wz*(UMxm*UNz-UMzm*UNx+CTG*(B*UNy-UMym))
         FV(6)=       -0.5D0*Wz*(UMym*UNx-UMxm*UNy+CTG*(B*UNz-UMzm))
@@ -109,7 +133,9 @@ C-- BNDRY ------ BOUNDARY CONDITIONS -- B(U,UX)=Z(T) ------------
         dimension U(NPDE),UX(NPDE),DZDT(NPDE),
      *   DBDU(NPDE,NPDE),DBDUX(NPDE,NPDE)
         real*8 T,X,U,UX,DZDT,DBDU,DBDUX
-        real*8 UN,UNx,UNy,UNz
+        real*8 UN,UNx,UNy,UNz, Uth
+        real*8 GNx,GNy,GNz, GMx,GMy,GMz, Gth
+        real*8 GGNx,GGNy,GGNz, GGMx,GGMy,GGMz, GGth
         real*8 ST,ST2,CT,CTM,CTM2,DD45,FTN,CTF,STF
         real*8 FTN4,FTN5,FTN7,FTNX4,FTNX5,C46,C56,C66,C266
         real*8 W,AF,DA
@@ -151,21 +177,30 @@ C         fix n vector length
           UNx=U(4)/UN
           UNy=U(5)/UN
           UNz=U(6)/UN
+          Uth=U(7)
 
-          ST=dsin(U(7))
+!          GMx = UX(1)
+!          GMy = UX(2)
+!          GMz = UX(3)
+          GNx = UX(4)
+          GNy = UX(5)
+          GNz = UX(6)
+          Gth = UX(7)
+
+          ST=dsin(Uth)
           ST2=2.0D0*ST
-          CT=dcos(U(7))
+          CT=dcos(Uth)
           CTM=1.0D0-CT
           CTM2=2.0D0*CTM
 
-          DD45=UNx*UX(5)-UX(4)*UNy
-          FTN=CTM*DD45-ST*UX(6)-UX(7)*UNz
+          DD45=UNx*GNy-GNx*UNy
+          FTN=CTM*DD45-ST*GNz-Gth*UNz
 
           CTF=CTM*FTN
           STF=ST*FTN
-          FTN4=CTM*UX(5)
-          FTN5=-CTM*UX(4)
-          FTN7=ST*DD45-CT*UX(6)
+          FTN4=CTM*GNy
+          FTN5=-CTM*GNx
+          FTN7=ST*DD45-CT*GNz
           FTNX4=-CTM*UNy
           FTNX5=CTM*UNx
           C46=CTM*UNx*UNz+UNy*ST
@@ -180,22 +215,22 @@ C         fix n vector length
           DBDUX(5,2)=DA
           DBDUX(6,3)=DA
 
-          DBDU(4,4)=2.0D0*UX(7)+CTF*UNz+C46*FTN4
-          DBDU(4,5)=CTM2*UX(6)+STF+C46*FTN5
-          DBDU(4,6)=-CTM2*UX(5)+CTF*UNx-C46*UX(7)
-          DBDU(4,7)=2.0D0*(CT*UX(4)+ST*(UNy*UX(6)-UX(5)*UNz))+
+          DBDU(4,4)=2.0D0*Gth+CTF*UNz+C46*FTN4
+          DBDU(4,5)=CTM2*GNz+STF+C46*FTN5
+          DBDU(4,6)=-CTM2*GNy+CTF*UNx-C46*Gth
+          DBDU(4,7)=2.0D0*(CT*GNx+ST*(UNy*GNz-GNy*UNz))+
      *     STF*UNx*UNz+UNy*CT*FTN+C46*FTN7
 
-          DBDU(5,4)=-CTM2*UX(6)-STF+C56*FTN4
-          DBDU(5,5)=2.0D0*UX(7)+CTF*UNz+C56*FTN5
-          DBDU(5,6)=CTM2*UX(4)+CTF*UNy-C56*UX(7)
-          DBDU(5,7)=2.0D0*(CT*UX(5)-ST*(UNx*UX(6)-UX(4)*UNz))+
+          DBDU(5,4)=-CTM2*GNz-STF+C56*FTN4
+          DBDU(5,5)=2.0D0*Gth+CTF*UNz+C56*FTN5
+          DBDU(5,6)=CTM2*GNx+CTF*UNy-C56*Gth
+          DBDU(5,7)=2.0D0*(CT*GNy-ST*(UNx*GNz-GNx*UNz))+
      *     STF*UNy*UNz-UNx*CT*FTN+C56*FTN7
 
-          DBDU(6,4)=CTM2*UX(5)+C66*FTN4
-          DBDU(6,5)=-CTM2*UX(4)+C66*FTN5
-          DBDU(6,6)=2.0D0*UNz*CTF+C266*UX(7)
-          DBDU(6,7)=2.0D0*(CT*UX(6)+ST*DD45)+STF*(UNz**2-1.0D0)+C66*FTN7
+          DBDU(6,4)=CTM2*GNy+C66*FTN4
+          DBDU(6,5)=-CTM2*GNx+C66*FTN5
+          DBDU(6,6)=2.0D0*UNz*CTF+C266*Gth
+          DBDU(6,7)=2.0D0*(CT*GNz+ST*DD45)+STF*(UNz**2-1.0D0)+C66*FTN7
 
           DBDUX(4,4)=ST2+C46*FTNX4
           DBDUX(4,5)=-CTM2*UNz+C46*FTNX5
@@ -211,9 +246,9 @@ C         fix n vector length
           DBDUX(6,5)=CTM2*UNx+C66*FTNX5
           DBDUX(6,6)=C266*ST
           DBDUX(6,7)=C266*UNz
-C          DBDU(7,4)=UX(4)         !!
-C          DBDU(7,5)=UX(5)         !!
-C          DBDU(7,6)=UX(6)         !!
+C          DBDU(7,4)=GNx         !!
+C          DBDU(7,5)=GNy         !!
+C          DBDU(7,6)=GNz         !!
 C          DBDUX(7,4)=UNx         !!
 C          DBDUX(7,5)=UNy         !!
 C          DBDUX(7,6)=UNz         !!
